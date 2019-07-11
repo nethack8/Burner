e4e8920fecc2985c835e26f796e3023981fcf422
nethack8 <51882235+nethack8@users.noreply.github.com>
2019-07-11 22:27:09 +0000

    Add files via upload

--- burn.sh
+++ burn.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-echo "Are you sure you want to overwrite "$1"?"
-read -p "To confirm, enter confirm: " confirm
-size=$( du -b $1 | egrep -o "[0-9]{1,40}" )
-if [ $confirm == "confirm" ]; then
-	echo "[i] Overwriting $1..."
-	for i in {1..8}
-	do
-		dd if=/dev/urandom of=$1 bs=$size status=progress count=1 &> /dev/null
-		echo "[i] $i/8 overwrites complete"
-	done
-	rm $1
-	echo "[i] Overwrite complete"
+
+help() {
+	echo "usage: ./$0 <file>"
+	exit 1
+}
+
+if [ $# != 1 ]; then
+	help
+fi
+
+# Check if linux shred utility is installed
+which shred &> /dev/null &
+if [ $? == 0 ]; then
+	shred -u $1
 else
-	echo "exiting"
-	sleep 3
-	exit
+	echo "Shred utility not found. Using dd utility instead..."
+	# Get size of file to be deleted
+	size=$( du -b $1 | egrep -o "[0-9]{1,40}" )
+	if [ $confirm == "confirm" ]; then
+		echo "[i] Overwriting $1..."
+		for i in {1..8}
+		do
+			dd if=/dev/urandom of=$1 bs=$size status=progress count=1 &> /dev/null
+			echo "[i] $i/8 overwrites complete"
+		done
+		rm $1
+		echo "[i] Overwrite complete"
+	else
+		echo "exiting"
+		sleep 3
+		exit
+	fi
 fi
